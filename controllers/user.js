18560c08188e1daf9c604bf9e78ea5a1ec8bba41
GerardoA <luceroriosg@gmail.com>
2018-03-28 16:28:52 -0700

    Video 23

--- controllers/user.js
+++ controllers/user.js
@@ -19,7 +19,6 @@ function saveUser(req, res){
 	user.email = params.email;
 	user.role = 'ROLE_ADMIN';
 	user.image = 'null';
-	console.log(user);
 	if(params.password){
 		//Encriptar contraseña y guardar datos
 		 bcrypt.hash(params.password, null, null, function(err, hash){
@@ -83,8 +82,61 @@ function loginUser(req, res){
 	});
 }
 
+function updateUser(req, res){
+	var userId = req.params.id;
+	var update = req.body;
+	User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+		if(err){
+			res.status(404).send({message: 'Error al actualizar el usuario'});
+		}
+		else{
+			if(!userUpdated){
+				res.status(404).send({message: 'Error al actualizar el usuario'});
+			}
+			else{
+				res.status(200).send({user: userUpdated});
+			}
+		}
+
+	});
+}
+
+function uploadImage(req, res){
+	var userId = req.params.id;
+	var file_name = 'No subido..';
+
+	if(req.files){
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('\\');
+		var file_name = file_split[2];
+
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+		console.log(file_name);
+		console.log(file_ext);
+
+		if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif'){
+			User.findByIdAndUpdate(userId, {image: file_name}, (err, userUpdated) =>{
+				if(err){
+					res.status(404).send({message: 'Error al actualizar el usuario'});
+				}
+				else{
+					res.status(200).send({user: userUpdated});
+				}
+			});
+		}
+		else{
+			res.status(200).send({message: 'Extension del archivo no valida'});
+		}
+	}else{
+		res.status(200).send({message: 'No ha subido ninguna imagen'});
+	}
+}
+
 module.exports = {
 	pruebas,
 	saveUser,
-	loginUser
+	loginUser,
+	updateUser, 
+	uploadImage
 };
--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -10,20 +10,16 @@ exports.ensureAuth = function(req, res, next){
 		return res.status(403).send({message: 'La peticion no tiene permisos'})
 	}
 	else{
-		
 		var token = req.headers.authorization.replace(/['"]+/g, '');
 		try{
 			var payload = jwt.decode(token, secret);
-
 			if(payload.exp <= moment().unix() ){
 				return res.status(401).send({message: 'El token ha expirado'})
 			}
 		}catch(ex){
-			console.log(ex);
+			//console.log(ex);
 			return res.status(403).send({message: 'El token no es valido'})
 		}
-
-
 		req.user = payload;
 		next();
 	}
