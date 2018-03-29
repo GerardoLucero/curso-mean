27bf2a3a0de91d44b7d0376fc098594250f88cdf
GerardoA <luceroriosg@gmail.com>
2018-03-29 12:32:52 -0700

    Get artist

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var user_routes = require('./routes/user');
+var artist_routes = require('./routes/artist');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //Cargar rutas 
 
 app.use('/api', user_routes);
+app.use('/api', artist_routes);
 
 module.exports = app;
