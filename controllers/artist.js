753f70c1dc63a81e169c3151d418a885fdf5e3ff
GerardoA <luceroriosg@gmail.com>
2018-03-29 13:22:14 -0700

    save artist

--- controllers/artist.js
+++ controllers/artist.js
@@ -9,6 +9,31 @@ function getArtist(req, res){
 	res.status(200).send({message: 'Metodo getArtist del controlador artist.js'});	
 }
 
+
+function saveArtist(req, res){
+	var artist = new Artist();
+	var params = req.body;
+
+	artist.name = params.name;
+	artist.description = params.description;
+	artist.image = 'null';
+	artist.save((err, artistStored) =>{
+		if(err){
+			res.status(500).send({artist: 'Error al guardar el artista'});	
+		}
+		else{
+			if(!artistStored){
+				res.status(404).send({artist: 'El artista no ha sido guardado'});	
+			}
+			else{
+				res.status(200).send({artist: artistStored});	
+			}			
+		}
+		
+	});
+
+}
 module.exports = {
-	getArtist
+	getArtist,
+	saveArtist
 };
--- routes/artist.js
+++ routes/artist.js
@@ -5,5 +5,7 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.get('/artist', md_auth.ensureAuth , ArtistController.getArtist);
+api.post('/artist', md_auth.ensureAuth , ArtistController.saveArtist);
+
 
 module.exports = api; 
