0e3cf9d2de55ef80664a4f18d5e9a0ef40329933
GerardoA <luceroriosg@gmail.com>
2018-04-05 17:25:15 -0600

    angular-cli

---
+++ client/src/app/models/artist.ts
@@ -0,0 +1,8 @@
+export class Artist{
+	public _
+	constructor(
+		public name: string,
+		public description: string, 
+		public image: string
+	){}
+}
---
+++ client/src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User{
+	public _
+	constructor(
+		public name: string,
+		public surname: string, 
+		public email: string 
+		public password: string,
+		public role:string,
+		public image: string
+	){}
+}
