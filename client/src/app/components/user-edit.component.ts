443e376dcde98ebcf4eeaf7d50bdde4240bd4ae8
Gerardo Arturo Lucero Ríos <luceroriosg@gmail.com>
2018-06-08 12:11:20 -0600

    upload image

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -39,6 +39,23 @@ export class UserEditComponent  implements OnInit{
 					//this.user = response.user;
 					localStorage.setItem('identity', JSON.stringify(this.user));
 					document.getElementById("identity_name").innerHTML = this.user.name;
+
+
+					if(!this.filesToUpload){
+						//Redirecion
+					}
+					else{
+						this.makeFileRequest(this.url+ 'upload-image-user/'+ this.user._id,[], this.filesToUpload).then(
+							(result: any) => {
+								this.user.image = result.image;
+
+								localStorage.setItem('identity', JSON.stringify(this.user));
+
+								console.log(this.user);
+							}
+						);
+					}
+
 					this.alertUpdate  = 'El usuario se ha actualizado correctamente';
 
 				}
@@ -51,6 +68,40 @@ export class UserEditComponent  implements OnInit{
 	          }
       		}
 		);
+	}
+
+	public filesToUpload: Array<File>;
+
+	fileChangeEvent(fileInput: any){
+		this.filesToUpload = <Array<File>> fileInput.target.files;
+	}
+
+	makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+		var token = this.token;
+
+		return new Promise(function(resolve, reject){
+			var formData: any = new formData();
+			var xhr = new XMLHttpRequest();
+
+			for(var i = 0; i < files.length; i++){
+				formData.append("image", file[i], files[i].name);
+			}
+
+			xhr.onreadystatechange = function(){
+				if(xhr.readyState == 4){
+					if(xhr.status == 200){
+						resolve(JSON.parse(xhr.response));
+					}
+					else{
+						reject(xhr.response);
+					}	
+				}
+			}
+			xhr.open('POST', url, true);
+			xhr.setRequestHeader('Authorization', token);
+			xhr.send(formData);
+		});
+
 	}
 
 
